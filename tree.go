cb2023bb833c259dd95a404bfc2420273e21663a
magician1 <kouta-k@mbm.nifty.com>
2013-09-03 00:30:38 +0900

    Children Struct is now OrderedMap

--- tree.go
+++ tree.go
@@ -1,5 +1,5 @@
 /*
-  The container/tree package implements tree container
+  The container package implements tree container
   this package does not serve binary-tree but ordered-tree.
   Node struct can be used as XMLNode etc...
 
@@ -16,43 +16,82 @@ import (
 var (
 	ERROR_REMOVE_CHILD = errors.New("You tried to remove a Node that is not in Node's children")
 	ERROR_NOT_CONTAINS = errors.New("NodeList does not contain the Node")
+	ERROR_LAST_CHILD   = errors.New("No younger sibling node")
 )
-
-//NodeList hanle Node's children
-type NodeList []*Node
 
 //NodePath is slice of pointers from root to a node.
 type NodePath []*Node
 
-//Index(node) returns a index number of the node in the Nodelist
-//if the node is not in the NodeList, this method returns error
-func (nl NodeList) Index(node *Node) (index int, err error) {
-	for i, n := range nl {
-		if n == node {
-			index = i
-			return
-		}
+//Children hanle Node's children
+type Children struct {
+	Nodes      []*Node
+	node_index map[*Node]int
+}
+
+func NewChildren() *Children {
+	children := new(Children)
+	children.node_index = make(map[*Node]int)
+	return children
+}
+
+//
+func (c *Children) Append(node *Node) {
+	_, ok := c.node_index[node]
+	if ok {
+		c.Remove(node)
 	}
-	index, err = -1, ERROR_NOT_CONTAINS
+	c.Nodes = append(c.Nodes, node)
+	c.node_index[node] = len(c.Nodes) - 1
+}
+
+//
+func (c *Children) Remove(node *Node) (err error) {
+	index, ok := c.node_index[node]
+	if !ok {
+		return ERROR_REMOVE_CHILD
+	}
+	new_list := make([]*Node, 0, 0)
+	for _, n := range c.Nodes[0:index] {
+		new_list = append(new_list, n)
+	}
+	for _, n := range c.Nodes[index+1:] {
+		new_list = append(new_list, n)
+	}
+	c.Nodes = new_list
+	delete(c.node_index, node)
+	return
+}
+
+//Index(node) returns a index number of the node in the children
+//if the node is not in the children, this method returns error
+func (c *Children) Index(node *Node) (index int, err error) {
+	log.Println("index:", c.node_index)
+	index, ok := c.node_index[node]
+	if !ok {
+		err = ERROR_NOT_CONTAINS
+	}
 	return
 }
 
 //Swap(i, j) swaps nodelist[i] and nodelist[j]
-func (nl NodeList) Swap(i, j int) {
-	nl[i], nl[j] = nl[j], nl[i]
+func (c *Children) Swap(i, j int) {
+	node1, node2 := c.Nodes[i], c.Nodes[j]
+	c.node_index[node1], c.node_index[node2] = c.node_index[node2], c.node_index[node1]
+	c.Nodes[i], c.Nodes[j] = c.Nodes[j], c.Nodes[i]
+
 }
 
 //SwapNodes(node1, node2) swaps the indice of node1 and node2
 //If eather node1 or node2 is not in the NodeList, this method returns error
-func (nl NodeList) SwapNodes(n1, n2 *Node) (err error) {
-	i1, err1 := nl.Index(n1)
-	i2, err2 := nl.Index(n2)
+func (c *Children) SwapNodes(n1, n2 *Node) (err error) {
+	i1, err1 := c.Index(n1)
+	i2, err2 := c.Index(n2)
 	if err1 != nil {
 		err = err1
 	} else if err2 != nil {
 		err = err2
 	} else {
-		nl.Swap(i1, i2)
+		c.Swap(i1, i2)
 	}
 	return
 }
@@ -60,14 +99,15 @@ func (nl NodeList) SwapNodes(n1, n2 *Node) (err error) {
 //Tree Node have its parent's pointer ,NodeList of its children and its Value
 type Node struct {
 	parent   *Node
-	children NodeList
+	children *Children
 	Value    Value
 }
 
 //NewNode function creates a Node and returns a pointer of the Node
 func NewNode(contents Value) *Node {
 	node := new(Node)
-	node.Value = &contents
+	node.children = NewChildren()
+	node.Value = contents
 	return node
 }
 
@@ -75,8 +115,8 @@ func (node *Node) Parent() *Node {
 	return node.parent
 }
 
-func (node *Node) Children() NodeList {
-	return node.children
+func (node *Node) Children() []*Node {
+	return node.children.Nodes
 }
 
 //Path() returns a slice of pointers from the root node to the node
@@ -104,36 +144,32 @@ func (node *Node) Path() NodePath {
 //Append a node to its children
 //If the new child is in another node's children,
 //it is removed from the previous parent's children
-func (node *Node) AppendChild(child *Node) (err error) {
-	node.children = append(node.children, child)
-	if parent := node.Parent(); parent != nil {
-		parent.RemoveChild(child)
-	}
+func (node *Node) AppendChild(child *Node) {
+	node.children.Append(child)
 	child.parent = node
-	return
 }
 
 //RemoveChild remove a child from the node's children
 //If the child is not in the children, its returns error.
 func (node *Node) RemoveChild(child *Node) (err error) {
-	capa := len(node.children)
-	newNodeList := make(NodeList, 0, capa)
-	for _, childnode := range node.children {
-		if childnode != child {
-			newNodeList = append(newNodeList, child)
-			child.parent = nil
-		}
-	}
-	if len(newNodeList) == capa {
-		err = ERROR_REMOVE_CHILD
+	return node.children.Remove(child)
+}
+
+//Sibling returns a younger sibling Node and error.
+//When youngest node calls Sibling(),
+//Sibling returns nil and not-nil error.
+func (node *Node) Sibling() (sibling *Node, err error) {
+	parent := node.Parent()
+	index, _ := parent.children.Index(node)
+	if index+1 < len(parent.children.Nodes) {
+		sibling = parent.children.Nodes[index+1]
 	} else {
-		node.children = newNodeList
+		err = ERROR_LAST_CHILD
 	}
 	return
 }
 
-/*
-Walk() is a generator.
+/*Walk() is a generator.
 It returns all nodes under the node.
 for node := range parent.Walk(){
   //do something
@@ -151,7 +187,7 @@ It returns nodes that funcion returns true
 func (node *Node) Filter(f func(*Node) bool) (ch chan *Node) {
 	ch = make(chan *Node)
 	go func() {
-		for _, child := range node.children {
+		for _, child := range node.children.Nodes {
 			if f(child) {
 				ch <- child
 			}
--- tree_test.go
+++ tree_test.go
@@ -5,6 +5,11 @@ import (
 	"testing"
 )
 
+func String(node *Node) string {
+	s, _ := node.Value.(string)
+	return s
+}
+
 func TestNode(t *testing.T) {
 	log.Println("test")
 	root := NewNode("root")
@@ -34,6 +39,45 @@ func TestNode(t *testing.T) {
 
 	for child := range root.Walk() {
 		log.Println("walk:", child)
+	}
+
+	log.Println("node_index:", a.children.node_index)
+	log.Println("nodes:", a.children.Nodes)
+	log.Println("parent:", b.Parent())
+	var n *Node
+	n, _ = b.Sibling()
+	log.Println("b sibling:", String(n))
+	n, _ = c.Sibling()
+	log.Println("c sibling:", String(n))
+	n, _ = d.Sibling()
+	log.Println("d sibling:", String(n))
+
+	for n := b; ; {
+		log.Println("sibling:", String(n))
+		next, err := n.Sibling()
+		if err != nil {
+			break
+		}
+		n = next
+
+	}
+
+	if a.Children()[0] != b {
+		t.Fatal("a's first child is not b")
+	}
+
+	if c.Children()[1] != g {
+		t.Fatal("c's second child is not g")
+	}
+
+	a.AppendChild(g)
+
+	if g.Parent() != a {
+		t.Fatal("g's parent is not a")
+	}
+
+	if a.Children()[4] != g {
+		t.Fatal("a's fourth children is not g")
 	}
 
 }
